b4b1eaa8106b3b8790dc3ac30acb9f63c5608760
Murali v <v.murali1818@gmail.com>
2024-07-13 22:09:24 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -2,16 +2,16 @@ import React from 'react';
 import './App.css';
 
 const products = [
-  { name: 'Product 1', logo: '/images/image.png' },
-  { name: 'Product 2', logo: '/images/image.png' },
-  { name: 'Product 3', logo: '/images/image.png' },
-  { name: 'Product 4', logo: '/images/image.png' },
-  { name: 'Product 5', logo: '/images/image.png' },
-  { name: 'Product 6', logo: '/images/image.png' },
-  { name: 'Product 7', logo: '/images/image.png' },
-  { name: 'Product 8', logo: '/images/image.png' },
-  { name: 'Product 9', logo: '/images/image.png' },
-  { name: 'Product 10', logo: '/images/image.png' },
+  { name: 'Product 1', logo: './images/image.png' },
+  { name: 'Product 2', logo: './images/image.png' },
+  { name: 'Product 3', logo: './images/image.png' },
+  { name: 'Product 4', logo: './images/image.png' },
+  { name: 'Product 5', logo: './images/image.png' },
+  { name: 'Product 6', logo: './images/image.png' },
+  { name: 'Product 7', logo: './images/image.png' },
+  { name: 'Product 8', logo: './images/image.png' },
+  { name: 'Product 9', logo: './images/image.png' },
+  { name: 'Product 10', logo: './images/image.png' },
 ];
 
 function App() {
